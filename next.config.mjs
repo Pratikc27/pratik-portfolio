4b66861a47612dc44cff0ec1ee9e7b8bac20d38e
D5_Pratik_69963 <pratikchoudharij@gmail.com>
2024-08-02 13:18:12 +0530

    page not render show 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/,
-          use: ['@svgr/webpack'],
-        });
-    
-        return config;
-      },
+  output: 'export',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
